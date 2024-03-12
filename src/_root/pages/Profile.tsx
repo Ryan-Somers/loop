badf7e52aef02a34b374cbfaa06512bfc801ba7b
Ryan Somers <rsomers333@gmail.com>
2024-03-12 13:48:29 -0400

    adding more profile

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,5 +1,18 @@
+import PostCard from "@/components/shared/PostCard";
+import { useUserContext } from "@/context/AuthContext";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useUserContext();
+
+  return (
+    <div className="profile-container">
+      <img src={user.imageUrl} className={`${"rounded-full w-24 h-24"}`} />
+      <div>
+        <p className="body-bold">{user.name}</p>
+        <p className="text-center small-regular text-light-3">@{user.username}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
